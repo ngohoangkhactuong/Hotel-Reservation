b49b60b433cead55cd5d52721f7cbda0136dd1bb
Ngo Hoang Khac Tuong <131318955+ngohoangkhactuong@users.noreply.github.com>
2024-08-01 16:10:53 +0700

    Add files via upload

---
+++ lakeSide/src/main/java/com/khactuong/lakesidehotel/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.khactuong.lakesidehotel.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ lakeSide/src/main/java/com/khactuong/lakesidehotel/service/IRoleService.java
@@ -0,0 +1,22 @@
+package com.khactuong.lakesidehotel.service;
+
+import java.util.List;
+
+import com.khactuong.lakesidehotel.model.entity.Role;
+import com.khactuong.lakesidehotel.model.entity.User;
+
+public interface IRoleService {
+    List<Role> getRoles();
+
+    Role createRole(Role theRole);
+
+    void deleteRole(Long id);
+
+    Role findByName(String name);
+
+    User removeUserFromRole(Long userId, Long roleId);
+
+    User assignRoleToUser(Long userId, Long roleId);
+
+    Role removeAllUsersFromRole(Long roleId);
+}
