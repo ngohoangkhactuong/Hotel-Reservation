f1eb877b1b5c8f90d8d4c4b586e3b0d1454d6c16
Ngo Hoang Khac Tuong <131318955+ngohoangkhactuong@users.noreply.github.com>
2024-08-12 10:37:17 +0700

    Delete lakeSide directory

--- lakeSide/src/main/java/com/khactuong/lakesidehotel/exception/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package com.khactuong.lakesidehotel.exception;
-
-public class InternalServerException extends RuntimeException {
-    public InternalServerException(String message) {
-        super(message);
-    }
-}
--- lakeSide/src/main/java/com/khactuong/lakesidehotel/exception/InvalidBookingRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.khactuong.lakesidehotel.exception;
-
-public class InvalidBookingRequestException extends RuntimeException {
-    public InvalidBookingRequestException(String message) {
-        super(message);
-    }
-}
