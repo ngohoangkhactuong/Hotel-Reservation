a1dc80f6feb7635ce4c289cf8641955f9ee7db8f
ngohoangkhactuong <khactuong1710@gmail.com>
2024-11-25 18:03:33 +0700

    run app

--- BE/src/main/java/com/khactuong/hotel/common/enumeration/ERoomType.java
+++ BE/src/main/java/com/khactuong/hotel/common/enumeration/ERoomType.java
@@ -3,13 +3,21 @@ package com.khactuong.hotel.common.enumeration;
 public enum ERoomType {
 
     Single("Single"),
+    Standard("Standard"),
     Double("Double"),
     Twin("Twin"),
     Triple("Triple"),
     Queen("Queen"),
     King("King"),
-    Suite("Suite");
-
+    Suite("Suite"),
+    Deluxe("Deluxe"),
+    Classic("Classic"),
+    Triple_Classic("Triple Classic"),
+    Business_Class("Business Class"),
+    Royal_Class("Royal Class"),
+    Superior_Ocean("Superior Ocean"),
+    Economy_Classic("Economy Classic");          
+    
     private final String NAME;
 
     ERoomType(String NAME) {
